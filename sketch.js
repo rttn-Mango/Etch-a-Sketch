e78cc291142599b0d3e7fb9bd7e1fd653d686f7e
rttn-Mango <khimmanga445@gmail.com>
2023-01-18 22:51:56 +0800

    Added the other functionalities

--- sketch.js
+++ sketch.js
@@ -1,9 +1,50 @@
-let sketchBoard = document.querySelector('.board');
-sketchBoard.style.gridTemplateColumns = "repeat(16, 1fr)";
-sketchBoard.style.gridTemplateRows = "repeat(16, 1fr)";
+let btnColor = 'black';
+let isBtnClicked = false;
 
-for (let i = 0; i < 256; i++) {
-    let boxes = document.createElement("div");
-    boxes.style.backgroundColor = 'blue';
-    sketchBoard.insertAdjacentElement("beforeend", boxes);
+function populateBoard(size) {
+    let sketchBoard = document.querySelector('.board');
+    sketchBoard.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    sketchBoard.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    const boardSize = size * size;
+
+    for (let i = 0; i < boardSize; i++) {
+        let boxes = document.createElement("div");
+        boxes.className = "boardPixel";
+        boxes.addEventListener("mousemove", () => {
+            if (isBtnClicked) {
+                if (btnColor === "random")
+                    boxes.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+
+                boxes.style.backgroundColor = btnColor;
+            }
+
+        });
+
+        boxes.style.backgroundColor = 'blue';
+        sketchBoard.insertAdjacentElement("beforeend", boxes);
+    }
 }
+
+function getButtonColor(color) {
+    btnColor = color;
+}
+
+function changeSize(size) {
+    populateBoard(size);
+}
+
+function resetBoard() {
+    let sketchBoard = document.querySelector('.board');
+    let boxes = document.querySelectorAll('.boardPixel');
+    boxes.forEach((div) => div.style.backgroundColor = 'blue');
+}
+
+document.querySelector('body').addEventListener('click', () => {
+    isBtnClicked = !isBtnClicked;
+    if (isBtnClicked)
+        document.querySelector('.sketchToggle').textContent = 'Sketch Mode: On';
+    else
+        document.querySelector('.sketchToggle').textContent = 'Sketch Mode: Off';
+});
+populateBoard(16);
